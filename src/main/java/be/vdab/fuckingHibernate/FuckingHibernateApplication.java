0bc859a93579893b1737aa5b415fe51dc705635e
[antdrioite] <[cnudde1@live.be]>
2019-04-04 10:48:28 +0200

    oops

---
+++ src/main/java/be/vdab/fuckingHibernate/FuckingHibernateApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.fuckingHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckingHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuckingHibernateApplication.class, args);
+	}
+
+}
